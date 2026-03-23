e4774dea0ffe6d7b357f0082aff29d98539019bd
shabareesha <arjunspy491@gmail.com>
2026-03-23 22:00:42 +0530

    added api-error.js, api-responce.js and constant.js

---
+++ src/utils/constant.js
@@ -0,0 +1,15 @@
+export const UserRoleEnum = {
+  ADMIN: "admin",
+  PROJECT_ADMIN: "project_admin",
+  MEMBERS: "members",
+};
+
+export const AvailableUserRole = Object.values(UserRoleEnum);
+
+export const TaskStatusEnum = {
+  TODO: "todo",
+  IN_PROGRESS: "in_progess",
+  DONE: "done",
+};
+
+export const AvailableTaskStatus = Object.values(AvailableTaskStatus);
