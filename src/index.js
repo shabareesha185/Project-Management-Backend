d93c7ae7c1a75a4224a3750619aded5f50044419
shabareesha <arjunspy491@gmail.com>
2026-03-23 19:06:42 +0530

    basic express configurations and CORS configurations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import app from "./app.js"
+
 dotenv.config({
-    path: "./.env",
-})
-console.log("New project BackEnd");
-console.log(`${process.env.NAME} : ${process.env.DATABASE}`);
+  path: "./.env",
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
